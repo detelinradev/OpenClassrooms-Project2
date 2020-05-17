7e5033d440d81d90ab4acc6ed9097f328d082123
Detelin Radev <clover88bg@yahoo.com>
2020-05-17 14:55:57 +0300

    An instance of ArrayList created to hold the data read from the source file, source file location changed to be in the root of the project

--- Project02/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,10 +1,15 @@
 package com.hemebiotech.analytics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnalyticsCounter {
 
 	public static void main(String args[]) {
 
 		SymptomReader reader = new SymptomReader(new ReadSymptomDataFromFile("symptoms.txt"));
+
+		List<String> data = new ArrayList<>(reader.getData());
 
 	}
 }
