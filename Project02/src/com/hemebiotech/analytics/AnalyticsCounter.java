71b8a69ce433e6aed65e67ca719a9b860bafe907
Detelin Radev <clover88bg@yahoo.com>
2020-05-17 16:25:36 +0300

    Added JavaDoc comments

--- Project02/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,8 +3,23 @@ package com.hemebiotech.analytics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>AnalyticsCounter</h1> The AnalyticsCounter program implements an
+ * application that reads symptoms from a file, one symptom per line, and write
+ * it in data structure by given requirements.
+ * <p>
+ *
+ * @author Detelin Radev
+ * @version 1.0
+ */
 public class AnalyticsCounter {
 
+	/**
+	 * This is the main method where we instantiate the classes used for reading,
+	 * processing and writing the data from the source file with the symptoms.
+	 * 
+	 * @param args Unused.
+	 */
 	public static void main(String args[]) {
 
 		SymptomReader reader = new SymptomReader(new ReadSymptomDataFromFile("symptoms.txt"));
