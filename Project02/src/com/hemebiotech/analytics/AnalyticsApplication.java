a5211b5c70919935c70eeec34e999c121e5d24b1
Detelin Radev <clover88bg@yahoo.com>
2020-06-02 17:16:27 +0300

    Added doc comments and class name changed to more appropriate

---
+++ Project02/src/com/hemebiotech/analytics/AnalyticsApplication.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+/**
+ * <h1>AnalyticsApplication</h1> The AnalyticsApplication program implements an
+ * application that reads symptoms from a file, one symptom per line, and write
+ * it in data structure by given requirements.
+ * <p>
+ *
+ * @author Detelin Radev
+ * @version 1.0
+ */
+public class AnalyticsApplication {
+
+	/**
+	 * This is the main method where we instantiate the classes used for reading,
+	 * processing and writing the data from the source file with the symptoms.
+	 * 
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
